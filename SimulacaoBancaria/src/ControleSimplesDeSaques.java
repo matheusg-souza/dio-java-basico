32eeada084137466a37f240701f51aca99e34f1d
Ryu <matheus.sh73@gmail.com>
2024-07-22 09:36:19 -0300

    add SimulacaoBancaria

---
+++ SimulacaoBancaria/src/ControleSimplesDeSaques.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ControleSimplesDeSaques {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double limiteDiario = scanner.nextDouble();
+        // Loop for para iterar sobre os saques
+        for (int i = 1; i < limiteDiario ; i++) {
+
+            double valorSaque = scanner.nextDouble();
+
+            if (valorSaque == 0) {
+                System.out.println("Transacoes encerradas.");
+                break; 
+            } else if (valorSaque > limiteDiario) {
+                System.out.println("Limite diario de saque atingido. Transacoes encerradas.");
+                break;
+            } else {
+                System.out.println("Saque realizado. Limite restante: " + (limiteDiario - valorSaque));
+            }
+        }
+        scanner.close();
+    }
+}
